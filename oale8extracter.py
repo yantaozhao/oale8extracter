b84b9c62b234cb618de10c848c923aec29bf36b8
Yantao Zhao <zhytsh15@126.com>
2018-07-02 16:17:02 +0800

    add Chinese definition

--- oale8extracter.py
+++ oale8extracter.py
@@ -15,20 +15,24 @@ from mdict_query import IndexBuilder
 def usage():
     scriptname = os.path.basename(__file__)
     hlp = f"""
-Extract sentences from OALE8. Result format: "word[TAB]en[TAB]zh"
+Extract sentences from OALE8. Result format: "word[TAB]defZh[TAB]en[TAB]zh"
 
 SYNOPSIS:
 {scriptname} [-i input.txt] [-o output.txt]
 
 OPTIONS:
 -i [input.txt]   : file of word list
--o [output.txt]  : result file. Default in one file, unless `-b` used.
+-o [output.txt]  : result file. Default in one file, unless `-b` used
+-k               : keep the original order in the input file
 -b  --break-file : break into files by leading letter
 -h  --help       : print this help and exit
 
 
 SEE ALSO:
-Use calibre to build e-book.
+- Sigil:
+https://sigil-ebook.com/
+
+- Use calibre to build e-book:
 https://manual.calibre-ebook.com/faq.html#how-do-i-convert-a-collection-of-html-files-in-a-specific-order
 ```html
 <html>
@@ -52,6 +56,15 @@ idxBuilder = None  # global variable
 
 
 def extractSentence(w):
+    """
+    extract content from dictionary file.
+    :param w: word
+    :return: list of specified contents, or `None` if example sentences not exist.
+             If the result is not None, its format is:
+               defZh: definition in Chinese. (Maybe None).
+               sentenceEn: example sentence in english. (Must exist).
+               sentenceZh: translation of above sentence. (Must exist).
+    """
     global idxBuilder
     if w is None or len(w) == 0 or idxBuilder is None:
         return None
@@ -61,24 +74,70 @@ def extractSentence(w):
         return None
 
     soup = BeautifulSoup(lookupResult[0], 'html5lib')
-    sentenceGroups = soup.find_all('span', 'x-g')  # group of example sentences
-    for group in sentenceGroups:
-        sentenceEn = group.find_all('span', 'x')
-        sentenceZh = group.find_all('span', 'oalecd8e_chn')
-        if len(sentenceEn) == 0 or len(sentenceZh) == 0:
-            continue
-        return [sentenceEn[0].get_text().replace('\t', ' ').strip(),
-                sentenceZh[0].get_text().replace('\t', ' ').strip()]
+    meaningGroups = soup.find_all('span', class_='n-g')
+    for group in meaningGroups:
+        # definition in Chinese:
+        defZh = None
+        defGroups = group.find_all('span', class_='def-g')
+        if len(defGroups) > 0:
+            defZhGroups = defGroups[0].find_all('span', class_='oalecd8e_chn')
+            if len(defZhGroups) > 0:
+                defZh = defZhGroups[0].get_text().replace('\t', ' ').strip()
+        # example sentences:
+        sentenceGroups = group.find_all('span', 'x-g')
+        for sGroup in sentenceGroups:
+            sentenceEn = sGroup.find_all('span', 'x')
+            sentenceZh = sGroup.find_all('span', 'oalecd8e_chn')
+            if len(sentenceEn) == 0 or len(sentenceZh) == 0:
+                continue  # ensure the two sentences exist
+            return [defZh,
+                    sentenceEn[0].get_text().replace('\t', ' ').strip(),
+                    sentenceZh[0].get_text().replace('\t', ' ').strip()]
     return None
+
+
+def formatContent(word, resultList):
+    defZh = resultList[0]
+    sentenceEn = resultList[1]
+    sentenceZh = resultList[2]
+
+    # plain text:
+    wordEnZhLineTxt = None
+    if defZh is None:
+        wordEnZhLineTxt = word + '\t' + ' ' + '\t' + sentenceEn + '\t' + sentenceZh
+    else:
+        wordEnZhLineTxt = word + '\t' + defZh + '\t' + sentenceEn + '\t' + sentenceZh
+
+    # html:
+    wordEnZhLineHtml = None
+    if defZh is None:
+        wordEnZhLineHtml = '<table width="100%">\n' + \
+                           ' <tr><td style="color:crimson; white-space: nowrap">' + word + '</td></tr>\n' + \
+                           ' <tr><td>' + re.sub(word, '<strong>\g<0></strong>', sentenceEn,
+                                                flags=re.IGNORECASE) + '</td></tr>\n' + \
+                           ' <tr><td>' + sentenceZh + '</td></tr>\n' + \
+                           '</table>' + '\n<hr style="border:none; height:1px; background-color:lightgray;" /><br />'
+    else:
+        wordEnZhLineHtml = '<table width="100%">\n' + \
+                           ' <tr><td style="color:crimson; white-space: nowrap">' + word + '</td>\n' + \
+                           '  <td style="text-align:center; font-size:70%; color:royalblue;">' + defZh + '</td></tr>\n' + \
+                           ' <tr><td colspan="2">' + re.sub(word, '<strong>\g<0></strong>', sentenceEn,
+                                                            flags=re.IGNORECASE) + '</td></tr>\n' + \
+                           ' <tr><td colspan="2">' + sentenceZh + '</td></tr>\n' + \
+                           '</table>' + '\n<hr style="border:none; height:1px; background-color:lightgray;" /><br />'
+
+    return [wordEnZhLineTxt,
+            wordEnZhLineHtml]
 
 
 def main():
     fileIn = 'input.txt'
     fileOut = ('output', '.txt')
+    keepOriginal = False
     breakFileFlag = False
     # parse command line args
     try:
-        opts, args = getopt.getopt(sys.argv[1:], 'hbi:o:', ['help', 'break-file'])
+        opts, args = getopt.getopt(sys.argv[1:], 'hkbi:o:', ['help', 'break-file'])
     except getopt.GetoptError as err:
         print('Error: %s!' % err)
         sys.exit(2)
@@ -90,6 +149,8 @@ def main():
             if len(fileOut[1]) == 0:
                 print('Error: Regular name of output file required! For example: output.txt')
                 sys.exit()
+        elif o == '-k':
+            keepOriginal = True
         elif o in ('-b', '--break-file'):
             breakFileFlag = True
         elif o in ('-h', '--help'):
@@ -103,8 +164,9 @@ def main():
     with open(fileIn, 'r') as fdIn:
         # words = fdIn.readlines()
         words = [w for w in [line.replace('\t', ' ').strip() for line in fdIn] if len(w) > 0]
-        words = list(set(words))
-        words.sort(key=str.lower)
+        if not keepOriginal:
+            words = list(set(words))
+            words.sort(key=str.lower)
         failWords = []
         print("Total words: %d." % len(words))
 
@@ -139,15 +201,11 @@ def main():
             # lookup word
             result = extractSentence(word)
             if result is not None:
-                wordEnZhLineTxt = word
-                wordEnZhLineHtml = '<p> ' + '<span style="color:#c00000"><strong>' + word + '</strong></span>'
-                for res in result:
-                    wordEnZhLineTxt += '\t' + res
-                    wordEnZhLineHtml += '<br/>' + re.sub(word, '<strong>\g<0></strong>', res, flags=re.IGNORECASE)
-                wordEnZhLineTxt += '\n'
-                wordEnZhLineHtml += '<br/> </p>\n'
-                fdOutTxt.write(wordEnZhLineTxt)
-                fdOutHtml.write(wordEnZhLineHtml)
+                markupResult = formatContent(word, result)
+                if markupResult[0] is not None:
+                    fdOutTxt.write(markupResult[0] + '\n')
+                if markupResult[1] is not None:
+                    fdOutHtml.write(markupResult[1] + '\n')
             else:
                 failWords.append(word)
                 print('[FAIL] ' + word)
